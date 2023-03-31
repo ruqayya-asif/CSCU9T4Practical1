e70cb402dda481114dc7208192264e7bb9f58081
ruqayya-asif <123831080+ruqayya-asif@users.noreply.github.com>
2023-03-31 22:26:16 +0400

    Create CycleEntry.java

---
+++ src/main/java/com/stir/cscu9t4practical1/CycleEntry.java
@@ -0,0 +1,29 @@
+package com.stir.cscu9t4practical1;
+
+public class CycleEntry extends Entry {
+
+	private String terrain;
+	private String tempo;
+	
+	public CycleEntry(String n, int d, int m, int y, int h, int min, int s, float dist, String terr, String tem) {
+		super(n, d, m, y, h, min, s, dist);
+		terrain = terr;
+		tempo = tem;
+	}
+
+	public String getTerrain() {
+		return terrain;
+	}
+
+	public String getTempo() {
+		return tempo;
+	}
+	
+	@Override
+	public String getEntry() {
+		String result = getName() + " cycled " + getDistance() + " km in " + getHour() + ":"
+				+ getMin() + ":" + getSec() + " on " + getDay() + "/"
+				+ getMonth() + "/" + getYear() + " on "+getTerrain()+ " at "+getTempo()+" tempo"+"\n";
+		return result;
+	}
+}
